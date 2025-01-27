cbf8f577ea616597d998a8972d693b5b06b945a9
Vedant Dubey <vedantdubey@Vedants-MacBook-Air.local>
2025-01-28 01:21:17 +0530

    updated og image

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,7 +24,7 @@ const Home: NextPage = () => {
         <title data-rh="true">Vedant Dubey - Software Developer</title>
         <meta data-rh="true" property="og:type" content="website"/>
         <meta data-rh="true" property="og:title" content="Vedant Dubey - Software Developer"/>
-        <meta data-rh="true" property="og:image" content="/favicon.ico"/>
+        <meta data-rh="true" property="og:image" content="/images/letter-v.png"/>
       </Head>
 
       {/* Header */}
